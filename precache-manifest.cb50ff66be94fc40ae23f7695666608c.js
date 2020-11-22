7328981b095866fdd48e98d7c0396c4f1a6daf0c
Jenna Chestnut <jennabot5000@gmail.com>
2020-11-21 19:15:04 -0600

    ready for deployment

---
+++ precache-manifest.cb50ff66be94fc40ae23f7695666608c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5de2ffd38dcacc2d1d0",
+    "url": "/book-search-app-ei/static/css/main.d78b6804.chunk.css"
+  },
+  {
+    "revision": "f5de2ffd38dcacc2d1d0",
+    "url": "/book-search-app-ei/static/js/main.f5de2ffd.chunk.js"
+  },
+  {
+    "revision": "b7b1d912abf39c73b9ca",
+    "url": "/book-search-app-ei/static/js/1.b7b1d912.chunk.js"
+  },
+  {
+    "revision": "90550873aec2e7f17f10",
+    "url": "/book-search-app-ei/static/js/runtime~main.90550873.js"
+  },
+  {
+    "revision": "6daa653a3fe4bb4cacbbc103702a19c2",
+    "url": "/book-search-app-ei/static/media/books.6daa653a.jpg"
+  },
+  {
+    "revision": "171751a6d7e70c6cc237a9ee2feb6720",
+    "url": "/book-search-app-ei/index.html"
+  }
+];
